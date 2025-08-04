2563e351ffe7702215dd5e7743161b863297b6c7
Hebert Tello <tellohebert@gmail.com>
2025-08-03 22:00:47 -0400

    Se crea variable para almacenar amigos

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Este array almacenará el nombre de todos los amigos
+let amigos = [];
