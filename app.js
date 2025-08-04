ae0a98f7a344ae5829c99822657b550bdcb4fb30
Hebert Tello <tellohebert@gmail.com>
2025-08-04 11:07:49 -0400

    Se crea funcion para agregar amigos y funcion para limpiar el campo de entrada

--- app.js
+++ app.js
@@ -2,3 +2,23 @@
 
 //Este array almacenará el nombre de todos los amigos
 let amigos = [];
+
+function agregarAmigo() {
+    //Guarda nombre de casilla de texto
+    let amigoNuevo = document.getElementById('amigo').value;
+    console.log(amigoNuevo);
+    //Condición si la casilla de texto esta vacia genera un alert
+    if (amigoNuevo == '') {
+        alert('Por favor, inserte un nombre.');
+
+    } //Si la casilla no está vacia, ingresa el nombre en el arreglo y limpia la caja
+        else {
+        amigos.push(amigoNuevo);
+        limpiarCaja();
+        console.log(amigos);
+    }
+    }
+
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
