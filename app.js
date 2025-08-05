49a05d0b6118aa7cd6e5656465ce4d6aa4afd1fc
Hebert Tello <tellohebert@gmail.com>
2025-08-05 01:08:16 -0400

    Se crea función para actualizar lista de amigos

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 //Este array almacenará el nombre de todos los amigos
 let amigos = [];
 
+//Esta función agrega los nombres en el arreglo
 function agregarAmigo() {
     //Guarda nombre de casilla de texto
     let amigoNuevo = document.getElementById('amigo').value;
@@ -16,9 +17,22 @@ function agregarAmigo() {
         amigos.push(amigoNuevo);
         limpiarCaja();
         console.log(amigos);
+        console.log(amigos.length);
+        visualizaLista(amigos);
     }
     }
 
+    //Limpia el cuadro de texto
 function limpiarCaja() {
     document.getElementById('amigo').value = '';
+}
+
+//Funcion que agrega cada elemento a la lista
+function visualizaLista(arreglo) {
+    let lista = document.createElement('li');
+    for (let index = 0; index < arreglo.length; index++) {
+        lista.textContent = arreglo[index];
+        document.getElementById('listaAmigos').appendChild(lista);
+    }
+    return;
 }
